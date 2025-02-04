24243bca0a4e707a9bfb120f3cf6451882ed3d2d
Alexey Zakharov <alexey.a.zakharov@gmail.com>
2025-02-04 13:32:35 +0300

    added prototype of EnterPasswordStep

--- src/pages/EnterPasswordStep/index.tsx
+++ src/pages/EnterPasswordStep/index.tsx
@@ -1,31 +1,23 @@
 import {Button, Container, Stack} from '@mui/material';
 import {useForm} from 'react-hook-form';
 import {TextFieldElement} from 'react-hook-form-mui';
-import {useNavigate} from 'react-router-dom';
-import {ROUTE__ENTER_PASSWORD_STEP} from '../../constants';
 
-export const EnterCodeStep = () => {
-  const navigate = useNavigate();
+export const EnterPasswordStep = () => {
   const {control, handleSubmit} = useForm({
     defaultValues: {
-      code: '',
+      password: '',
     },
-  });
+  })
 
   return (
     <Container maxWidth="sm">
-      <form
-        onSubmit={handleSubmit(data => {
-          console.log(data);
-          navigate(ROUTE__ENTER_PASSWORD_STEP);
-        })}
-        noValidate
-        style={{marginTop: 60}}
-      >
+      <form onSubmit={handleSubmit((data) => {
+        console.log(data);
+      })} noValidate style={{marginTop: 60}}>
         <Stack spacing={2}>
           <TextFieldElement
-            name={'code'}
-            label={'Введите код'}
+            name={'password'}
+            label={'Пароль'}
             control={control}
             required
             fullWidth
