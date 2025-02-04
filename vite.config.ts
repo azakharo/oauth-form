55b51486cd5f6f07f1454eeb3dffbdeaae5fa778
Alexey Zakharov <alexey.a.zakharov@gmail.com>
2025-02-04 11:24:00 +0300

    initial commit: just copied the starter

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+﻿/// <reference types="vitest/config" />
+
+import * as path from 'path';
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+
+export default () => {
+  return defineConfig({
+    plugins: [
+      react(),
+      // svgr options: https://react-svgr.com/docs/options/
+      svgr({svgrOptions: {icon: true}}),
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+    server: {
+      open: true,
+      port: 4000,
+    },
+    test: {
+    },
+  });
+};
