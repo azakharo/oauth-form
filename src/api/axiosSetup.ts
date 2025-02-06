ac4c718e4f4ee1d5eb7240cd685d83152116d35b
Alexey Zakharov <alexey.a.zakharov@gmail.com>
2025-02-06 12:09:17 +0300

    added showing error on the 1st step

--- src/api/axiosSetup.ts
+++ src/api/axiosSetup.ts
@@ -1,19 +1,19 @@
-import axios from 'axios';
+import axios, {isAxiosError} from 'axios';
 
 export const axi = axios.create({
   baseURL: '/api/v0/auth',
 });
 
-// TODO extract error detail and throw new Error
-// axi.interceptors.response.use(
-//   (response) => response,
-//   (error: unknown) => {
-//     if (isAxiosError(error)) {
-//       if (error.response?.status === 401) {
-//         redirectToLogin();
-//       }
-//     }
-//
-//     return Promise.reject(error);
-//   },
-// );
+axi.interceptors.response.use(
+  response => response,
+  (error: unknown) => {
+    if (isAxiosError(error)) {
+      const errorMessage = error.response?.data?.detail?.msg as string;
+      if (errorMessage) {
+        return Promise.reject(new Error(errorMessage));
+      }
+    }
+
+    return Promise.reject(error);
+  },
+);
