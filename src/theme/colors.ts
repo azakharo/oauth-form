cceaa28122e85bd02e6a51e6334f94568e146707
Alexey Zakharov <alexey.a.zakharov@gmail.com>
2025-02-07 12:54:03 +0300

    added theme colors

---
+++ src/theme/colors.ts
@@ -0,0 +1,22 @@
+export const COLOR__BLACK = '#000';
+export const COLOR__MAIN_BLACK = '#171717';
+export const COLOR__GRAY = '#636170';
+export const COLOR__WHITE = '#fff';
+export const COLOR__LIGHT_GRAY = '#98979F';
+export const COLOR__LINE = '#CBCDD8';
+
+// Standard backgrounds
+export const COLOR__BACK = '#EDEEF3';
+export const COLOR__LIGHT_BACK = '#F6F6F6';
+export const COLOR__OFF_WHITE = COLOR__WHITE;
+
+// Main colors
+export const COLOR__PRIMARY = '#FFC106';
+export const COLOR__SECONDARY = '#003C96';
+export const COLOR__ERROR = '#DA1136';
+export const COLOR__WARNING = '#FE8414';
+export const COLOR__INFO = '#31A6E6'; // not used in Figma
+export const COLOR__SUCCESS = '#58B80D';
+export const COLOR__TEXT_PRIMARY = COLOR__MAIN_BLACK;
+export const COLOR__TEXT_SECONDARY = COLOR__GRAY;
+export const COLOR__TEXT_DISABLED = '#EDEDED';
