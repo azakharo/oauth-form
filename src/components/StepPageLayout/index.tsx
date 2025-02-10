3d7a46930dd97487f243d381526ae13a9bde47ca
Alexey Zakharov <alexey.a.zakharov@gmail.com>
2025-02-10 16:44:25 +0300

    changed the responsive layout impl

--- src/components/StepPageLayout/index.tsx
+++ src/components/StepPageLayout/index.tsx
@@ -1,9 +1,9 @@
 import {FC, PropsWithChildren} from 'react';
-import {Box, Container, Typography} from '@mui/material';
+import {Box, Typography} from '@mui/material';
 
 import Logo from './logo.svg?react';
 
-import {useIsDesktop, useIsTablet} from '@/hooks/responsive';
+import {useIsDesktop} from '@/hooks/responsive';
 
 interface Props {
   title: string;
@@ -13,47 +13,59 @@ export const StepPageLayout: FC<PropsWithChildren<Props>> = ({
   title,
   children,
 }) => {
-  const isTablet = useIsTablet();
   const isDesktop = useIsDesktop();
 
-  let backgroundColor;
+  const content = (
+    <>
+      {/* This container is necessary to align the logo and title center horizontally */}
+      <Box display="flex" flexDirection="column" alignItems="center">
+        <Logo width={210} height={'100%'} style={{marginBottom: 32}} />
+
+        <Typography
+          mb={4}
+          sx={{
+            fontSize: 28,
+            fontWeight: 500,
+            lineHeight: '28px',
+          }}
+        >
+          {title}
+        </Typography>
+      </Box>
+
+      {children}
+    </>
+  );
+
   if (isDesktop) {
-    backgroundColor = 'green';
-  } else if (isTablet) {
-    backgroundColor = 'blue';
-  } else {
-    backgroundColor = 'red';
+    return (
+      <Box display="flex" sx={{height: '100dvh'}}>
+        <Box flex="1 1 0" sx={{backgroundColor: 'green'}}>
+          1
+        </Box>
+
+        <Box
+          flex="1 1 0"
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          p={2}
+        >
+          <Box flex="0 1 400px">{content}</Box>
+        </Box>
+      </Box>
+    );
   }
 
   return (
-    <Container style={{maxWidth: 400}}>
-      <Box
-        sx={{
-          height: '100dvh',
-          backgroundColor,
-        }}
-        display="flex"
-        alignItems="center"
-      >
-        <Box width="100%">
-          <Box display="flex" flexDirection="column" alignItems="center">
-            <Logo width={210} height={'100%'} style={{marginBottom: 32}} />
-
-            <Typography
-              mb={4}
-              sx={{
-                fontSize: 28,
-                fontWeight: 500,
-                lineHeight: '28px',
-              }}
-            >
-              {title}
-            </Typography>
-          </Box>
-
-          {children}
-        </Box>
-      </Box>
-    </Container>
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      sx={{height: '100dvh'}}
+      p={4}
+    >
+      <Box flex="0 1 400px">{content}</Box>
+    </Box>
   );
 };
