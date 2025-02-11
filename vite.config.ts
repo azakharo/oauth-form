d7780c35fde32f32dc5a2424730c85a5d51c60f1
Alexey Zakharov <alexey.a.zakharov@gmail.com>
2025-02-11 16:03:43 +0300

    Revert "rem proxying"
    This reverts commit 4aedc266cf196dd25ee93be5c12c743eb3418cfd.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 ﻿/// <reference types="vitest/config" />
 
 import * as path from 'path';
-import {defineConfig} from 'vite';
+import {defineConfig, loadEnv} from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
-export default defineConfig(() => {
+export default defineConfig(({mode}) => {
+  const {VITE_API_URL} = loadEnv(mode, process.cwd());
+
   return {
     plugins: [
       react(),
@@ -20,6 +22,13 @@ export default defineConfig(() => {
     server: {
       open: true,
       port: 4000,
+      proxy: {
+        '^/api': {
+          target: VITE_API_URL,
+          changeOrigin: true,
+          secure: true,
+        },
+      },
     },
     test: {},
   };
