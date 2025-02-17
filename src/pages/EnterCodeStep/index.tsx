877d8d87e5c39b875e4339ce412c4bd3c76b293b
Alexey Zakharov <alexey.a.zakharov@gmail.com>
2025-02-17 20:49:29 +0300

    enter sms code step: added resending phone number and count down

--- src/pages/EnterCodeStep/index.tsx
+++ src/pages/EnterCodeStep/index.tsx
@@ -1,17 +1,21 @@
+import {useState} from 'react';
 import {useForm} from 'react-hook-form';
 import {TextFieldElement} from 'react-hook-form-mui';
 import {useNavigate} from 'react-router-dom';
 import {yupResolver} from '@hookform/resolvers/yup';
-import {Button, Stack} from '@mui/material';
+import {Alert, Box, Button, Stack, Typography} from '@mui/material';
+import useCountDown from 'ahooks/es/useCountDown';
+import useMount from 'ahooks/es/useMount';
 import useRequest from 'ahooks/es/useRequest';
 import {object, string} from 'yup';
 
 import {CodeFormat} from './CodeFormat';
 
-import {enterSmsCode} from '@/api';
+import {enterPhone, enterSmsCode} from '@/api';
 import {StepPageLayout} from '@/components/StepPageLayout';
 import {ROUTE__ENTER_PASSWORD_STEP} from '@/constants';
 import {useAuthData} from '@/contexts/AuthDataContext';
+import {COLOR__LIGHT_GRAY} from '@/theme/colors';
 
 const codeRegExp = /^\d{4}$/;
 
@@ -23,8 +27,14 @@ const v8nSchema = object().shape({
 
 export const EnterCodeStep = () => {
   const navigate = useNavigate();
-  const {code, setCode, tokenToEnterSmsCode, setTokenToEnterPassword, phone} =
-    useAuthData();
+  const {
+    code,
+    setCode,
+    tokenToEnterSmsCode,
+    setTokenToEnterPassword,
+    phone,
+    setTokenToEnterSmsCode,
+  } = useAuthData();
 
   const {control, handleSubmit, setError} = useForm({
     mode: 'onBlur',
@@ -33,6 +43,45 @@ export const EnterCodeStep = () => {
       code,
     },
   });
+
+  /////////////////////////////////////////////////////////
+  // Send SMS retry
+
+  const [countDownTargetDate, setCountDownTargetDate] = useState<number>();
+
+  const startCountDown = () => {
+    setCountDownTargetDate(Date.now() + 60_000);
+  };
+
+  const [countdown] = useCountDown({
+    targetDate: countDownTargetDate,
+  });
+
+  useMount(() => {
+    startCountDown();
+  });
+
+  const {
+    loading: isSendingPhone,
+    run: sendPhone,
+    error: errorSendingPhone,
+  } = useRequest(enterPhone, {
+    manual: true,
+    onSuccess: token => {
+      setTokenToEnterSmsCode(token);
+      startCountDown();
+    },
+    onError: () => {
+      startCountDown();
+    },
+  });
+
+  const resendSmsCode = () => {
+    sendPhone(phone);
+  };
+
+  // Send SMS retry
+  /////////////////////////////////////////////////////////
 
   const {loading, run, params} = useRequest(enterSmsCode, {
     manual: true,
@@ -88,6 +137,41 @@ export const EnterCodeStep = () => {
             Далее
           </Button>
         </Stack>
+
+        {!!countdown && (
+          <Typography
+            variant="button"
+            align="center"
+            display="block"
+            sx={{color: COLOR__LIGHT_GRAY}}
+            mt="22px"
+          >
+            {`Отправить код повторно через 00:${Math.round(countdown / 1000)
+              .toString()
+              .padStart(2, '0')}`}
+          </Typography>
+        )}
+
+        {countdown === 0 && (
+          <Button
+            variant="text"
+            color="secondary"
+            fullWidth
+            sx={{display: 'block', marginTop: '8px'}}
+            onClick={resendSmsCode}
+            disabled={isSendingPhone}
+          >
+            <Typography variant="button" align="center">
+              Отправить код повторно
+            </Typography>
+          </Button>
+        )}
+
+        {errorSendingPhone && (
+          <Box mt={4}>
+            <Alert severity="error">{errorSendingPhone.message}</Alert>
+          </Box>
+        )}
       </form>
     </StepPageLayout>
   );
